eb6176ab7c35321595f405f2b5459b4021289b72
POLISETTI KAILASH BHARAT <pkailashbharat@gmail.com>
2026-08-21 04:18:35 +0530

    Time: 0 ms (100%), Space: 24.2 MB (19.73%) - LeetHub

---
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        int n=nums.size();
+        if(n<=2) return nums;
+        vector<int> arr1,arr2;
+        arr1.push_back(nums[0]);
+        arr2.push_back(nums[1]);
+        for(int i=2;i<n;i++){
+            if(arr1.back()>arr2.back()){
+                arr1.push_back(nums[i]);
+            }
+            else arr2.push_back(nums[i]);
+        }
+        vector<int> result;
+        for(auto i:arr1) result.push_back(i);
+        for(auto i:arr2) result.push_back(i);
+
+        return result;
+    }
+};
